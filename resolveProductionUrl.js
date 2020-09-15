47fff6f9d55502b426a1bc0266864d2f74acdfb9
mike connor <mike@rocketnumber9.org>
2020-09-15 18:10:15 -0500

    2nd comit

---
+++ resolveProductionUrl.js
@@ -0,0 +1,6 @@
+const previewSecret = 'skmFaPWYUPjHZFAqDlza7T7vehiAuDSxNkqqkQ8cLGfaHZyf9k07hEAfkawg7ERzmYyLut0sfUYKPEeYjBM7GcaCsjhYfrj31LEbp6WKUKY8SeWNTUuhtCuqRAisVbm6QxQhPbLC6ERJKsaGDzlyFKGDpxolgoDCzLhE8byLGat5bYIa3yH4' // Copy the string you used for SANITY_PREVIEW_SECRET
+const projectUrl = 'http://localhost:3000'
+
+export default function resolveProductionUrl(document) {
+  return `${projectUrl}/api/preview?secret=${previewSecret}&slug=${document.slug.current}`
+}
